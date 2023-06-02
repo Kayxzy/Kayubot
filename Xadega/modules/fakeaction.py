605876053c1c091029033d29e1288174e913cf50
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2023-06-02 18:06:14 +0700

    Update fakeaction.py

--- Xadega/modules/fakeaction.py
+++ Xadega/modules/fakeaction.py
@@ -1,54 +1,125 @@
-from asyncio import sleep
+from asyncio import sleep 
 
-from pyrogram import filters
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.types import ChatPermissions, ChatPrivileges, Message
+  
 
-from Xadega import ubot
-from Xadega.config import *
-from Xadega.modules.bot import add_command_help
-from Xadega.utils.data import *
-from Xadega.utils.misc import *
+ from pyrogram import Client, enums, filters 
 
-commands = {
-    "ftyping": enums.ChatAction.TYPING,
-    "fstop": enums.ChatAction.CANCEL,
-}   
+ from pyrogram.raw import functions 
 
-@ubot.on_message(filters.command(list(commands), PREFIX) & filters.me)
-async def _(client, message: Message):
-    cmd = message.command[0]
-    try:
-        sec = int(message.command[1])
-        if sec > 60:
-            sec = 60
-    except:
-        sec = None
-    await message.delete()
-    action = commands[cmd]
-    try:
-        if action != "screenshot":
-            if sec and action != enums.ChatAction.CANCEL:
-                await client.send_chat_action(chat_id=message.chat.id, action=action)
-                await sleep(sec)
-            else:
-                return await client.send_chat_action(
-                    chat_id=message.chat.id, action=action
-                )
-                await sleep(sec)
-    except Exception as e:
-        return await client.send_message(
-            message.chat.id,
-            f"**ERROR:** `{e}`",
-            reply_to_message_id=ReplyCheck(message),
-        )
+ from pyrogram.types import Message 
 
+  
 
-add_command_help(
-    "fakeaction",
-    [
-        ["ftyping [detik]", "Menampilkan Pengetikan Palsu dalam obrolan."],
-        ["fstop", "Memberhentikan tindakan palsu dalam obrolan."],
-    ],
-)
+ from config import CMD_HANDLER as cmd 
 
+ from otanproject.helpers.PyroHelpers import ReplyCheck 
+
+  
+
+ from .help import add_command_help 
+
+  
+
+ commands = { 
+
+     "ftyping": enums.ChatAction.TYPING, 
+
+     "fvideo": enums.ChatAction.RECORD_VIDEO, 
+
+     "faudio": enums.ChatAction.RECORD_AUDIO, 
+
+     "fround": enums.ChatAction.RECORD_VIDEO_NOTE, 
+
+     "fphoto": enums.ChatAction.UPLOAD_PHOTO, 
+
+     "fsticker": enums.ChatAction.CHOOSE_STICKER, 
+
+     "fdocument": enums.ChatAction.UPLOAD_DOCUMENT, 
+
+     "flocation": enums.ChatAction.FIND_LOCATION, 
+
+     "fgame": enums.ChatAction.PLAYING, 
+
+     "fcontact": enums.ChatAction.CHOOSE_CONTACT, 
+
+     "fstop": enums.ChatAction.CANCEL, 
+
+     "fscreen": "screenshot", 
+
+ } 
+
+  
+
+  
+
+ @Client.on_message(filters.command(list(commands), cmd) & filters.me) 
+
+ async def fakeactions_handler(client: Client, message: Message): 
+
+     cmd = message.command[0] 
+
+     try: 
+
+         sec = int(message.command[1]) 
+
+         if sec > 60: 
+
+             sec = 60 
+
+     except: 
+
+         sec = None 
+
+     await message.delete() 
+
+     action = commands[cmd] 
+
+     try: 
+
+         if action != "screenshot": 
+
+             if sec and action != enums.ChatAction.CANCEL: 
+
+                 await client.send_chat_action(chat_id=message.chat.id, action=action) 
+
+                 await sleep(sec) 
+
+             else: 
+
+                 return await client.send_chat_action( 
+
+                     chat_id=message.chat.id, action=action 
+
+                 ) 
+
+         else: 
+
+             for _ in range(sec if sec else 1): 
+
+                 await client.send( 
+
+                     functions.messages.SendScreenshotNotification( 
+
+                         peer=await client.resolve_peer(message.chat.id), 
+
+                         reply_to_msg_id=0, 
+
+                         random_id=client.rnd_id(), 
+
+                     ) 
+
+                 ) 
+
+                 await sleep(0.1) 
+
+     except Exception as e: 
+
+         return await client.send_message( 
+
+             message.chat.id, 
+
+             f"ERROR: {e}", 
+
+             reply_to_message_id=ReplyCheck(message), 
+
+         )
